265c8febe94cf2d455ed2f817f2df49b63864aa6
ANKIT MODANWAL <166202965+ankitmodanwall@users.noreply.github.com>
2025-11-23 22:21:14 +0530

    Time: 25 ms (39.99%), Space: 47.9 MB (28.09%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int calculate(String s) {
+       Stack<Integer> st = new Stack<>();
+       char c = '+';
+       long n = 0;
+
+       for(int i = 0; i<s.length(); i++){
+        char c1  = s.charAt(i);
+        if(Character.isDigit(c1)){
+            n = n*10 + (c1 -'0');
+        }
+        if (!Character.isDigit(c1) && !Character.isWhitespace(c1)|| i == s.length() -1){
+        if(c == '+') {
+            st.push((int)n);
+        }
+        else if(c == '-')
+        {
+        st.push((int)(-n));
+        }
+        else if(c == '*') {
+        st.push(st.pop() * (int)n);
+        }
+        else if (c == '/') 
+        {st.push(st.pop() / (int)n);    
+        }   
+         c = c1;
+        n =0; 
+    
+    }}
+    int res = 0;
+    while(!st.isEmpty()){
+        res += st.pop();
+       } 
+       return res;
+    }
+}
