661d2748c7f3796373f0328ec0c73623df57e5d5
ANKIT MODANWAL <166202965+ankitmodanwall@users.noreply.github.com>
2025-11-30 19:24:50 +0530

    Time: 1 ms (91.36%), Space: 42.8 MB (6.05%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,25 +1,24 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x<2){
+        if(x<2)
         return x;
-        }
+
         int left = 1;
         int right = x/2;
-
-        while (left <= right) {
-            int mid = left + (right -left )/2;
+        while(left <= right ){
+            int mid = left + (right-left)/2; //oveflow
             long sq = (long)mid * mid;
             if(sq == x){
                 return mid;
             }
-           else if(sq <x){
-            left = mid+1;
-           }
-           else {
-            right = mid -1;
-           }
-        } 
-
-    return right;
+            else if(sq < x){
+                left = mid +1;
+            }
+            else {
+                right = mid -1;
+            }
+            
+        }
+        return right;
     }
 }
