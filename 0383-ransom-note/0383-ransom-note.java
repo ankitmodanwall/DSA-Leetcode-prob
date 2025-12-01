b5c051775878294da0795d09086896b083c8757e
ANKIT MODANWAL <166202965+ankitmodanwall@users.noreply.github.com>
2025-12-02 03:20:32 +0530

    Time: 1 ms (99.69%), Space: 46.6 MB (22.67%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if(ransomNote.length() > magazine.length())
+        return false;
+        int[] alphabet_cnt = new int[26];
+        for(char c : magazine.toCharArray())
+        alphabet_cnt[c-'a']++;
+
+        for(char c: ransomNote.toCharArray()){
+            if(alphabet_cnt[c-'a'] == 0)
+            return false;
+        alphabet_cnt[c-'a']--;
+        }
+        return true;
+    }
+}
