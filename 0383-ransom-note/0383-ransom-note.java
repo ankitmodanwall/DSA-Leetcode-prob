7806b0e16cff0a1ac394e1e320d583aaf8f38d1c
ANKIT MODANWAL <166202965+ankitmodanwall@users.noreply.github.com>
2025-12-02 03:20:45 +0530

    Time: 1 ms (99.69%), Space: 46.3 MB (31.53%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        if(ransomNote.length() > magazine.length())
-        return false;
-        int[] alphabet_cnt = new int[26];
-        for(char c : magazine.toCharArray())
-        alphabet_cnt[c-'a']++;
-
-        for(char c: ransomNote.toCharArray()){
-            if(alphabet_cnt[c-'a'] == 0)
+        if (ransomNote.length() > magazine.length())
             return false;
-        alphabet_cnt[c-'a']--;
+        int[] alphabet_cnt = new int[26];
+        for (char c : magazine.toCharArray())
+            alphabet_cnt[c - 'a']++;
+
+        for (char c : ransomNote.toCharArray()) {
+            if (alphabet_cnt[c - 'a'] == 0)
+                return false;
+            alphabet_cnt[c - 'a']--;
         }
         return true;
     }
