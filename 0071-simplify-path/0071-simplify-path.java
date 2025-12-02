3d761b90b7212181032be74cc3093371874dddd1
ANKIT MODANWAL <166202965+ankitmodanwall@users.noreply.github.com>
2025-12-02 16:42:20 +0530

    Time: 9 ms (10.99%), Space: 45 MB (20.33%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,21 +1,23 @@
 import java.util.*;
+
 class Solution {
     public String simplifyPath(String path) {
-        
+
         String[] s1 = path.split("/+");
-        Stack <String> stack = new Stack();
-        for(String t : s1){
-            if(t.equals("") || t.equals("."))
-        continue;
+        Stack<String> stack = new Stack();
+        for (String t : s1) {
+            if (t.equals("") || t.equals(".")) {
+                continue;
+            }
+            if (t.equals("..")) {
+                if (!stack.isEmpty())
+                    stack.pop();
+            }
 
-        if(t.equals("..")){
-            if(!stack.isEmpty()) stack.pop();
-        }
+            else {
 
-        else {
-
-        stack.push(t);
-        }
+                stack.push(t);
+            }
         }
         return "/" + String.join("/", stack);
 
