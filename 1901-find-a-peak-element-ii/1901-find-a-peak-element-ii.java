223f30e9e72c0ef7dcbff26b39d2c5f24ff3ba88
ANKIT MODANWAL <166202965+ankitmodanwall@users.noreply.github.com>
2025-12-12 02:20:59 +0530

    Time: 0 ms (100%), Space: 116.2 MB (21.73%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
@@ -1,23 +1,23 @@
 class Solution {
     public int[] findPeakGrid(int[][] mat) {
         int start = 0;
-        int end = mat[0].length-1;
-        while(start<=end){
-            int maxrow = 0, midcol = start + (end-start)/2;
+        int end = mat[0].length - 1;
+        while (start <= end) {
+            int maxrow = 0, midcol = start + (end - start) / 2;
 
-            for(int row = 0; row < mat.length; row++){
-                maxrow = mat[row][midcol]>= mat[maxrow][midcol]? row : maxrow;
+            for (int row = 0; row < mat.length; row++) {
+                maxrow = mat[row][midcol] >= mat[maxrow][midcol] ? row : maxrow;
             }
-            boolean leftisbig = midcol -1>= start && mat[maxrow][midcol -1] > mat[maxrow][midcol];
-            boolean rightisbig = midcol + 1<= end && mat[maxrow][midcol +1]>mat[maxrow][midcol];
+            boolean leftisbig = midcol - 1 >= start && mat[maxrow][midcol - 1] > mat[maxrow][midcol];
+            boolean rightisbig = midcol + 1 <= end && mat[maxrow][midcol + 1] > mat[maxrow][midcol];
 
-            if(!leftisbig && !rightisbig)
-            return new int[]{maxrow,midcol};
+            if (!leftisbig && !rightisbig)
+                return new int[] { maxrow, midcol };
 
-            else if(rightisbig)
-            start = midcol+1;
+            else if (rightisbig)
+                start = midcol + 1;
             else
-            end = midcol-1;
+                end = midcol - 1;
         }
         return null;
     }
