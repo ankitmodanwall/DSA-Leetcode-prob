5928924a512283773bf49bc7b3349d02bd203562
ANKIT MODANWAL <166202965+ankitmodanwall@users.noreply.github.com>
2025-12-17 09:43:15 +0530

    Time: 6 ms (99.47%), Space: 48 MB (26.49%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1, high = 1_000_000_000;
+        while(low<high){
+        int mid = low + (high-low)/2;
+        if(isPossible(piles,h,mid))
+        high = mid;
+        else
+        low = mid+1;
+        }
+        return low;
+    }
+    private boolean isPossible(int[] piles,int h, int k){
+        int time = 0;
+        for(int p : piles){
+            time += (p-1)/k +1;
+            if(time>h);
+        }
+        return time <=h;
+           }
+}
