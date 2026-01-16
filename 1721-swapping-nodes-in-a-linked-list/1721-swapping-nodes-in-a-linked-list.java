fc939dfbc77507ac4f1959efcea2f644678c0728
ANKIT MODANWAL <166202965+ankitmodanwall@users.noreply.github.com>
2026-01-16 21:59:54 +0530

    Time: 3 ms (12.63%), Space: 118.4 MB (95.84%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -8,25 +8,26 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+ // sol 2 using Arraylist to store nodes 
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-     ListNode n1 = head;
-     List<ListNode> n2 = new ArrayList<>();
+        ListNode n1 = head;
+        List<ListNode> n2 = new ArrayList<>();
 
-     // storing nodes ---
-    while(n1 != null){
-        n2.add(n1);
-        n1 = n1.next;
-    }
-    // swp
+        // storing nodes ---
+        while (n1 != null) {
+            n2.add(n1);
+            n1 = n1.next;
+        }
+        // swp
 
-    int len = n2.size();
-    int temp = n2.get(k-1).val;
-    n2.get(k-1).val = n2.get(len-k).val;
-    n2.get(len - k).val = temp;
+        int len = n2.size();
+        int temp = n2.get(k - 1).val;
+        n2.get(k - 1).val = n2.get(len - k).val;
+        n2.get(len - k).val = temp;
 
-    return head;
-
+        return head;
 
     }
 }
