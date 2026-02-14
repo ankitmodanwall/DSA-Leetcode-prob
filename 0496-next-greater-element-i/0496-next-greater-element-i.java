63299708f723f2635cfca7b02e61e658158e10b8
ANKIT MODANWAL <166202965+ankitmodanwall@users.noreply.github.com>
2026-02-15 01:29:58 +0530

    Time: 3 ms (92.73%), Space: 45 MB (72.65%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i<nums1.length;i++){
+        int num1current = nums1[i];
+        int index = -1;
+        for(int j = 0; j<nums2.length;j++){
+        int curr = nums2[j];
+            if(curr == num1current){
+        index = j;
+        break;
+            }}
+        int ans = -1;
+        for(int j = index +1; j <nums2.length;j++){
+            if(num1current < nums2[j]){
+                ans = nums2[j];
+                break;
+            }
+        }
+        list.add(ans);
+        }
+        int[] res = new int[list.size()];
+        for(int j = 0; j<list.size(); j++){
+            res[j] = list.get(j);
+        }
+        return res;
+        }
+}
