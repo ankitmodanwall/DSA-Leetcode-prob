95428340492fb18b5d20ba45e5d21647f8c245f2
ANKIT MODANWAL <166202965+ankitmodanwall@users.noreply.github.com>
2026-02-19 01:42:12 +0530

    Time: 72 ms (28.32%), Space: 57.9 MB (44.53%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -1,54 +1,53 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        int[] nextsm  = nextsmaller(arr);
+        int[] nextsm = nextsmaller(arr);
         int[] prevsm = prevsmaller(arr);
         int mod = 1000000007;
         long cnt = 0;
-        for(int i = 0; i<arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             int first = i - prevsm[i];
-            int second = nextsm[i] -i;
-            long prod = ( 1L * first * second) % mod;
+            int second = nextsm[i] - i;
+            long prod = (1L * first * second) % mod;
             prod = (prod * arr[i]) % mod;
             cnt = (cnt + prod) % mod;
-            
+
         }
         return (int) cnt;
 
-
     }
-    private int[] nextsmaller (int[] arr){
+
+    private int[] nextsmaller(int[] arr) {
         Stack<Integer> st = new Stack<>();
         int[] ans = new int[arr.length];
-        for(int i = arr.length-1; i>=0; i--){
-            while(!st.isEmpty() && arr[st.peek()] > arr[i]){
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
                 st.pop();
             }
-            if(st.isEmpty()){
+            if (st.isEmpty()) {
                 ans[i] = arr.length;
-            }
-            else {
+            } else {
                 ans[i] = st.peek();
             }
 
-        st.push(i);
+            st.push(i);
         }
         return ans;
     }
-    private int[] prevsmaller (int[] arr){
+
+    private int[] prevsmaller(int[] arr) {
         Stack<Integer> st = new Stack<>();
         int[] ans = new int[arr.length];
-        for(int i = 0; i<arr.length; i++){
-            while(!st.isEmpty() && arr[st.peek()] >= arr[i]){
+        for (int i = 0; i < arr.length; i++) {
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
                 st.pop();
             }
-            if(st.isEmpty()){
+            if (st.isEmpty()) {
                 ans[i] = -1;
-            }
-            else {
+            } else {
                 ans[i] = st.peek();
             }
 
-        st.push(i);
+            st.push(i);
         }
         return ans;
     }
