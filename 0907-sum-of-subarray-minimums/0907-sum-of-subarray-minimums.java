3460a5d3c3be4811d3438d0d93a1ebe1235a1a53
ANKIT MODANWAL <166202965+ankitmodanwall@users.noreply.github.com>
2026-02-19 01:41:22 +0530

    Time: 73 ms (20.03%), Space: 57.8 MB (51.55%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        int[] nextsm  = nextsmaller(arr);
+        int[] prevsm = prevsmaller(arr);
+        int mod = 1000000007;
+        long cnt = 0;
+        for(int i = 0; i<arr.length; i++){
+            int first = i - prevsm[i];
+            int second = nextsm[i] -i;
+            long prod = ( 1L * first * second) % mod;
+            prod = (prod * arr[i]) % mod;
+            cnt = (cnt + prod) % mod;
+            
+        }
+        return (int) cnt;
+
+
+    }
+    private int[] nextsmaller (int[] arr){
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[arr.length];
+        for(int i = arr.length-1; i>=0; i--){
+            while(!st.isEmpty() && arr[st.peek()] > arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                ans[i] = arr.length;
+            }
+            else {
+                ans[i] = st.peek();
+            }
+
+        st.push(i);
+        }
+        return ans;
+    }
+    private int[] prevsmaller (int[] arr){
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[arr.length];
+        for(int i = 0; i<arr.length; i++){
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                ans[i] = -1;
+            }
+            else {
+                ans[i] = st.peek();
+            }
+
+        st.push(i);
+        }
+        return ans;
+    }
+
+}
