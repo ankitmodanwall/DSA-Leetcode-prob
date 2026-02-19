ee76a59f215e56212019d7d27c675c7ea0b5113f
ANKIT MODANWAL <166202965+ankitmodanwall@users.noreply.github.com>
2026-02-20 02:48:58 +0530

    Time: 4 ms (81.58%), Space: 47.2 MB (42.93%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,32 +1,30 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> stack = new Stack<>();
-        for(int i :asteroids){
+        for (int i : asteroids) {
             boolean flag = true;
-            while(!stack.isEmpty() && i < 0 && stack.peek() >0){
-                if(-i>stack.peek()){
+            while (!stack.isEmpty() && i < 0 && stack.peek() > 0) {
+                if (-i > stack.peek()) {
                     stack.pop();
-                }
-                else if(-i <stack.peek()){
-                   flag = false;
+                } else if (-i < stack.peek()) {
+                    flag = false;
+                    break;
+                } else {
+                    flag = false;
+                    stack.pop();
                     break;
                 }
-                    else{
-                        flag = false;
-                        stack.pop();
-                        break;
-                    }
             }
-            if(flag){
+            if (flag) {
                 stack.push(i);
 
             }
         }
-    int []arr = new int[stack.size()];
-    for(int i = arr.length-1; i>=0;i--){
-        arr[i] = stack.pop();
-    }
-return arr;
+        int[] arr = new int[stack.size()];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            arr[i] = stack.pop();
+        }
+        return arr;
 
     }
 }
