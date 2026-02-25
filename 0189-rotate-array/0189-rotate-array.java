798a630d07c7864fcaff53bb944851a481e7765c
ANKIT MODANWAL <166202965+ankitmodanwall@users.noreply.github.com>
2026-02-26 00:46:10 +0530

    Time: 0 ms (100%), Space: 61.3 MB (55.33%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,6 +1,27 @@
+import java.util.*;
+
 class Solution {
     public void rotate(int[] nums, int k) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        
+        k = k % nums.length;
+        int n = nums.length;
+        // checking elements 
+        // time complexity -> O(n) + O(k) + O(n-k) = O(n)
+        //  Space complexity -> O(1)
+
+        reverseNum(nums, 0, n - 1);
+        reverseNum(nums, 0, k - 1);
+        reverseNum(nums, k, n - 1);
+    }
+
+    public void reverseNum(int[] nums, int start, int end) {
+
+        // swapping the elements 
+        while (start <= end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
