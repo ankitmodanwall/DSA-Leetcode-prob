106caeecce852a17b10bb4e913c770371760342d
ANKIT MODANWAL <166202965+ankitmodanwall@users.noreply.github.com>
2026-04-03 02:49:06 +0530

    Time: 0 ms (100%), Space: 43.1 MB (77.68%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -1,28 +1,27 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
         int ones = 0, zeros = 0;
-        for (int std : students){
-            if(std == 0){
+        for (int std : students) {
+            if (std == 0) {
                 zeros++;
-               } else{
-                ones ++;
+            } else {
+                ones++;
             }
         }
-        for(int sandwich : sandwiches)
-    {
-        if(sandwich ==0){
-            if(zeros==0 )
-            return ones;
-            zeros--;
-        }else
-                    {
+        for (int sandwich : sandwiches) {
+            if (sandwich == 0) {
+                if (zeros == 0)
+                    return ones;
+                zeros--;
+            } else {
 
-        if(ones == 0)
-        return zeros;
-    
-        ones--;
+                if (ones == 0)
+                    return zeros;
 
+                ones--;
+
+            }
+        }
+        return 0;
     }
-    }
-    return 0;
-    }}
+}
