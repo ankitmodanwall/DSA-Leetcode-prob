9fd1037d27bebb941cf93383ff0cfdc923baa22f
ANKIT MODANWAL <166202965+ankitmodanwall@users.noreply.github.com>
2026-04-04 02:40:19 +0530

    Time: 5 ms (85.82%), Space: 46.6 MB (20.68%) - LeetHub

---
+++ 0641-design-circular-deque/0641-design-circular-deque.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class MyCircularDeque {
+
+    private int[] v;
+    private int front, back, size, capacity;
+
+    public MyCircularDeque(int k) {
+        v = new int[k];
+        front = 0;
+        back = 0;
+        size = 0;
+        capacity = k;
+    }
+
+    public boolean insertFront(int value) {
+        if (isFull()) return false;
+
+        front = (front - 1 + capacity) % capacity;
+        v[front] = value;
+        size++;
+        return true;
+    }
+
+    public boolean insertLast(int value) {
+        if (isFull()) return false;
+
+        v[back] = value;
+        back = (back + 1) % capacity;
+        size++;
+        return true;
+    }
+
+    public boolean deleteFront() {
+        if (isEmpty()) return false;
+
+        front = (front + 1) % capacity;
+        size--;
+        return true;
+    }
+
+    public boolean deleteLast() {
+        if (isEmpty()) return false;
+
+        back = (back - 1 + capacity) % capacity;
+        size--;
+        return true;
+    }
+
+    public int getFront() {
+        if (isEmpty()) return -1;
+        return v[front];
+    }
+
+    public int getRear() {
+        if (isEmpty()) return -1;
+        return v[(back - 1 + capacity) % capacity];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == capacity;
+    }
+}
