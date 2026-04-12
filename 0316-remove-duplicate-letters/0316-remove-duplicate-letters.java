675593e97db941ace5e4f9e36b2981346f30a541
ANKIT MODANWAL <166202965+ankitmodanwall@users.noreply.github.com>
2026-04-13 00:58:34 +0530

    Time: 4 ms (23.02%), Space: 44.6 MB (14.95%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -3,26 +3,26 @@ class Solution {
     public String removeDuplicateLetters(String s) {
         Stack<Character> stack = new Stack<>();
         Set<Character> s1 = new HashSet<>();
-        Map<Character,Integer> last = new HashMap<>();
-        for(int i = 0; i<s.length(); i++){
-            last.put(s.charAt(i),i) ;     
-            
-             }
-        for(int i = 0; i<s.length(); i++){
+        Map<Character, Integer> last = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            last.put(s.charAt(i), i);
+
+        }
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if(!s1.contains(c)){
-                while(!stack.isEmpty() && c < stack.peek() && i< last.get(stack.peek())){
+            if (!s1.contains(c)) {
+                while (!stack.isEmpty() && c < stack.peek() && i < last.get(stack.peek())) {
                     s1.remove(stack.pop());
                 }
                 s1.add(c);
                 stack.push(c);
             }
         }
-            StringBuilder result = new StringBuilder();
-            for(char c : stack){
-                result.append(c);
-            }
-            return result.toString();
+        StringBuilder result = new StringBuilder();
+        for (char c : stack) {
+            result.append(c);
+        }
+        return result.toString();
 
     }
 }
