a30c260719fbabe79830c40d44bc6b189d23687f
ANKIT MODANWAL <166202965+ankitmodanwall@users.noreply.github.com>
2026-04-21 12:15:59 +0530

    Time: 3 ms (29.27%), Space: 45.2 MB (93.37%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,27 +14,30 @@
  * }
  */
 class Solution {
-        int preIndex = 0;
+    int preIndex = 0;
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return build(preorder, inorder, 0, inorder.length -1);
+        return build(preorder, inorder, 0, inorder.length - 1);
     }
-    TreeNode build(int[] preorder, int[] inorder, int start, int end){
-        if(start>end) return null;
-        
+
+    TreeNode build(int[] preorder, int[] inorder, int start, int end) {
+        if (start > end)
+            return null;
+
         int rootval = preorder[preIndex++];
         TreeNode root = new TreeNode(rootval);
 
         int inIndex = find(inorder, start, end, rootval);
 
-        root.left = build(preorder, inorder, start, inIndex -1);
-        root.right = build(preorder , inorder, inIndex+1, end);
+        root.left = build(preorder, inorder, start, inIndex - 1);
+        root.right = build(preorder, inorder, inIndex + 1, end);
         return root;
     }
 
-    int find(int[] arr, int s, int e, int key){
-        for(int i = s; i<=e; i++) {
-            if(arr[i] == key) return i;
+    int find(int[] arr, int s, int e, int key) {
+        for (int i = s; i <= e; i++) {
+            if (arr[i] == key)
+                return i;
         }
         return -1;
     }
